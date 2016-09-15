d641c92dd5ef20b55f2af182823576933c29e9e8
fernandog <fernandog@users.noreply.github.com>
2016-09-15 08:55:09 -0300

    [Local only] Adds retry decorator to getURL

--- medusa/helpers/__init__.py
+++ medusa/helpers/__init__.py
@@ -25,6 +25,7 @@ import uuid
 import warnings
 import xml.etree.ElementTree as ET
 import zipfile
+from functools import wraps
 from itertools import cycle, izip
 
 import adba
@@ -1241,6 +1242,45 @@ def prepare_cf_req(session, request):
                     u' Error: {0}', error)
 
 
+def retry(exception_to_check, tries=4, delay=3, backoff=2):
+    """Retry calling the decorated function using an exponential backoff.
+
+    http://www.saltycrane.com/blog/2009/11/trying-out-retry-decorator-python/
+    original from: http://wiki.python.org/moin/PythonDecoratorLibrary#Retry
+
+    :param exception_to_check: the exception to check. may be a tuple of
+        exceptions to check
+    :type exception_to_check: Exception or tuple
+    :param tries: number of times to try (not retry) before giving up
+    :type tries: int
+    :param delay: initial delay between retries in seconds
+    :type delay: int
+    :param backoff: backoff multiplier e.g. value of 2 will double the delay
+        each retry
+    :type backoff: int
+    """
+    def deco_retry(f):
+
+        @wraps(f)
+        def f_retry(*args, **kwargs):
+            mtries, mdelay = tries, delay
+            while mtries > 1:
+                try:
+                    return f(*args, **kwargs)
+                except exception_to_check, e:
+                    msg = "%s, Retrying in %d seconds..." % (str(e), mdelay)
+                    log.warning(msg)
+                    time.sleep(mdelay)
+                    mtries -= 1
+                    mdelay *= backoff
+            return f(*args, **kwargs)
+
+        return f_retry  # true decorator
+
+    return deco_retry
+
+
+@retry(requests.exceptions.RequestException)
 def get_url(url, post_data=None, params=None, headers=None, timeout=30, session=None, **kwargs):
     """Return data retrieved from the url provider."""
     response_type = kwargs.pop(u'returns', u'response')
